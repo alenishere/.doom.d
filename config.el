7ef5469a9b9254dc7a4cbcc3913a4121ba6d449b
Alen Alex Ninan <alenishere@gmail.com>
2021-08-05 17:26:55 +0530

    Org-roam UI keybindings

--- config.el
+++ config.el
@@ -845,20 +845,29 @@ you're done. This can be called from an external shell script."
 ;;       ("^\\*org-roam: " ; node dedicated org-roam buffer
 ;;        :side right :width .33 :height .5 :ttl nil :modeline nil :quit nil :slot 2)))
 ;;   )
+
 ;; Org-roam-ui
 ;; ------------------------------------------------------------------------------
 (use-package! org-roam-ui
-    :after org-roam ;; or :after org
-;;    :hook
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t))
+  :after org-roam
+  ;; or :after org
+  ;;    :hook
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t)
+  (map! (:map org-mode-map
+         :localleader
+         :prefix ("ms" . "UI")
+         "o" #'org-roam-ui-mode
+         "f" #'org-roam-ui-follow-mode
+         ))
+  )
 
 ;; Org-roam-bibtex
 ;; ------------------------------------------------------------------------------
